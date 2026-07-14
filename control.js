bb9795498e489b84f935f8e863c44b63bf526994
ChromaEsport <max-kaya@live.fr>
2026-07-14 11:28:34 +0200

    Add files via upload

---
+++ control.js
@@ -0,0 +1,109 @@
+import {
+db,
+doc,
+setDoc
+} from "./firebase.js";
+
+
+
+let score1 = 0;
+let score2 = 0;
+
+
+
+function addScore1(){
+
+score1++;
+
+document.getElementById("score1").innerHTML = score1;
+
+}
+
+
+function removeScore1(){
+
+if(score1 > 0){
+score1--;
+}
+
+document.getElementById("score1").innerHTML = score1;
+
+}
+
+
+
+function addScore2(){
+
+score2++;
+
+document.getElementById("score2").innerHTML = score2;
+
+}
+
+
+function removeScore2(){
+
+if(score2 > 0){
+score2--;
+}
+
+document.getElementById("score2").innerHTML = score2;
+
+}
+
+
+
+async function sendMatch(){
+
+
+const matchRef = doc(
+db,
+"matches",
+"current"
+);
+
+
+
+await setDoc(matchRef,{
+
+team1:
+document.getElementById("team1").value,
+
+
+team2:
+document.getElementById("team2").value,
+
+
+score1: score1,
+
+
+score2: score2,
+
+
+map:
+document.getElementById("map").value,
+
+
+mapCurrent:
+Number(document.getElementById("mapCurrent").value),
+
+
+mapTotal:
+Number(document.getElementById("mapTotal").value)
+
+
+});
+
+
+alert("Score envoyé !");
+
+
+}
+
+window.addScore1 = addScore1;
+window.removeScore1 = removeScore1;
+
+window.addScore2 = addScore2;
+window.removeScore2 = removeScore2;
+
+window.sendMatch = sendMatch;
---
+++ firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import { 
+getFirestore,
+doc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHMDcbowJ2XfD6_RbmMiPj8epjfoXMA1M",
+  authDomain: "chroma-caster.firebaseapp.com",
+  projectId: "chroma-caster",
+  storageBucket: "chroma-caster.firebasestorage.app",
+  messagingSenderId: "657595541032",
+  appId: "1:657595541032:web:270a4394e10a8d2b1df71d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+
+export {
+db,
+doc,
+setDoc
+};
