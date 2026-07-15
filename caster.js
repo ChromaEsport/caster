0ee8fcdec5cc58a3eecf65875783990a0b26a4fa
ChromaEsport <max-kaya@live.fr>
2026-07-15 16:44:49 +0200

    Create caster.js

---
+++ caster.js
@@ -0,0 +1,36 @@
+import {
+    db,
+    doc,
+    onSnapshot
+} from "./firebase.js";
+
+
+
+const matchRef = doc(
+    db,
+    "matches",
+    "current"
+);
+
+
+
+onSnapshot(matchRef, (doc) => {
+
+
+    const data = doc.data();
+
+
+    if (!data) return;
+
+
+
+    document.getElementById("casters").textContent =
+    data.casters || "PSEUDO CASTER";
+
+
+
+    document.getElementById("event").textContent =
+    data.event || "NOM EVENEMENT";
+
+
+});
