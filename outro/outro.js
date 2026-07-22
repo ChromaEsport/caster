a8afc47019ed91f28a363c962d4938070f8ba9d4
ChromaEsport <max-kaya@live.fr>
2026-07-22 09:34:00 +0200

    Update outro.js

--- outro/outro.js
+++ outro/outro.js
@@ -1,1 +1,157 @@
+import {
+    db,
+    doc,
+    onSnapshot
+} from "../firebase.js";
 
+
+
+const matchRef = doc(
+    db,
+    "matches",
+    "current"
+);
+
+
+
+onSnapshot(matchRef, (doc) => {
+
+
+    const data = doc.data();
+
+
+    if (!data) return;
+
+
+
+    document.querySelector(".event").textContent =
+        data.event || "FACEIT MATCH";
+
+
+
+    document.querySelector(".team1").textContent =
+        data.team1 || "CHROMA";
+
+
+
+    document.querySelector(".team2").textContent =
+        data.team2 || "ADVERSAIRE";
+
+
+});
+
+
+const welcome = document.querySelector(".welcome");
+
+const line1 = document.getElementById("welcome-line1");
+
+const line2 = document.getElementById("welcome-line2");
+
+
+
+const text1 = "BIENVENUE SUR LE LIVE";
+
+const text2 = "LE MATCH VA COMMENCER";
+
+
+
+let i = 0;
+
+let j = 0;
+
+
+
+function writeLine1(){
+
+
+    if(i < text1.length){
+
+        line1.textContent += text1.charAt(i);
+
+        i++;
+
+        setTimeout(writeLine1,80);
+
+    }
+
+    else {
+
+        setTimeout(writeLine2,500);
+
+    }
+
+}
+
+
+
+function writeLine2(){
+
+
+    if(j < text2.length){
+
+        line2.textContent += text2.charAt(j);
+
+        j++;
+
+        setTimeout(writeLine2,80);
+
+    }
+
+}
+
+
+
+function showWelcome(){
+
+
+    line1.textContent="";
+
+    line2.textContent="";
+
+
+    i=0;
+
+    j=0;
+
+
+    welcome.classList.add("show");
+
+
+    writeLine1();
+
+
+
+    setTimeout(()=>{
+
+
+        hideWelcome();
+
+
+    },8000);
+
+
+}
+
+
+
+function hideWelcome(){
+
+
+    welcome.classList.remove("show");
+
+
+
+    setTimeout(()=>{
+
+
+        showWelcome();
+
+
+    },3000);
+
+
+}
+
+
+
+setTimeout(showWelcome,1000);
