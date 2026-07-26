17625f3f41e1d2170c06985ac99ba8c2e3dd7def
ChromaEsport <max-kaya@live.fr>
2026-07-26 20:19:05 +0200

    Update draft.js

--- draft/draft.js
+++ draft/draft.js
@@ -20,7 +20,7 @@ onSnapshot(draftRef, (docSnap) => {
     document.getElementById("map1Img").src =`../maps/${data.map1}.png`;
 
     document.getElementById("map2Name").textContent = data.map2;
-    document.getElementById("map2Img").src = `caster/maps/${data.map2}.png`;
+    document.getElementById("map2Img").src = `../maps/${data.map2}.png`;
 
     document.getElementById("map3Name").textContent = data.map3;
     document.getElementById("map3Img").src = `../maps/${data.map3}.png`;
