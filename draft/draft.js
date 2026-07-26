03c1d591784637a806adb6ea6213c75485f25a37
ChromaEsport <max-kaya@live.fr>
2026-07-26 19:41:44 +0200

    Update draft.js

--- draft/draft.js
+++ draft/draft.js
@@ -1,1 +1,25 @@
-console.log("Draft chargé");
+import {
+    db,
+    doc,
+    onSnapshot
+} from "../firebase.js";
+
+const draftRef = doc(
+    db,
+    "draft",
+    "current"
+);
+
+onSnapshot(draftRef, (docSnap) => {
+
+    const data = docSnap.data();
+
+    if (!data) return;
+
+    document.getElementById("map1").textContent = data.map1 || "MAP 1";
+    document.getElementById("map2").textContent = data.map2 || "MAP 2";
+    document.getElementById("map3").textContent = data.map3 || "MAP 3";
+    document.getElementById("map4").textContent = data.map4 || "MAP 4";
+    document.getElementById("map5").textContent = data.map5 || "MAP 5";
+
+});
