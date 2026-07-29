3ebe1bc024f02c26cc130ede719e48bbf27f017b
ChromaEsport <max-kaya@live.fr>
2026-07-29 12:01:09 +0200

    Update draft.js

--- draft/draft.js
+++ draft/draft.js
@@ -178,10 +178,10 @@ if(heroData){
 }
 
     // Affichage
-    slot.classList.remove("play");
-   if(heroContainer){
+     if(heroContainer){
 
     heroContainer.classList.add("show");
+    slot.classList.remove("play");
     slot.classList.add("show");
     slot.classList.add("banned");
     slot.classList.add("glow");
